a3bf3b83f9a37e924ddde9c7ce8ad115a9e0799a
Geoffrey Clark <mr@gffryclrk.com>
2022-05-13 06:14:28 -0400

    Binary counting of elephant solution appears to work

--- elephant_counting.py
+++ elephant_counting.py
@@ -2,6 +2,8 @@
 Solve for combinations of letters
 This time, by binary counting """
 
+import pdb
+
 def combine_lists(letters, inclusions):
     """ This fn takes two lists and returns a string
     of letters from the first list if their respective
@@ -13,3 +15,12 @@ def combine_lists(letters, inclusions):
 
     return ''.join(o)
 
+def combinations(word):
+    """ Given a word, generate combinations of letters """
+    letters = list(word)
+    combinations = int(('1' * len(letters)), 2) # Binary representation of all letters, ie 111 for 'cat'
+
+    for a in range(1, combinations+1): # range fn returns [a, b) whereas I want (a, b]
+#       pdb.set_trace()
+        bit_map = bin(a)[2:].zfill(len(letters))
+        yield combine_lists(letters, [int(i) for i in list(bit_map)])
--- test/test_elephant_counting.py
+++ test/test_elephant_counting.py
@@ -1,5 +1,5 @@
 import unittest
-from elephant_counting import combine_lists
+from elephant_counting import combine_lists, combinations
 
 class TestElephantCounting(unittest.TestCase):
 
@@ -9,6 +9,12 @@ class TestElephantCounting(unittest.TestCase):
         s = combine_lists(['a', 'b', 'c'], [1, 0, 0])
         self.assertEqual('a', s)
 
+
+    def test_combination_generator(self):
+        """ Consume the generator a few times and see what happens """
+
+        for combo in combinations('elephant'):
+            print(combo)
 
 if __name__ == '__main__':
     unittest.main()
